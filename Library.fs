76175b80626d4018fe2767f13b161dfe574f2978
Dan <daniel.gomme@yahoo.co.uk>
2022-03-16 13:08:18 +0000

    Initial commit

---
+++ Library.fs
@@ -0,0 +1,19 @@
+﻿namespace FlourPower
+
+module Recipes =
+    type BakersMath = { 
+        flourType: string
+        hydration: float
+        salt: float
+    }
+    let hello name =
+        printfn "Hello %s" name
+
+    let recipe = {
+        flourType = "hey"
+        hydration = 60.0
+        salt = 2.0
+    }
+
+    let flour = recipe.flourType
+    hello "me"
